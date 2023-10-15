6fa19f734391ede020711b42ec3d20c92065c03e
deepak suthar <deepaksuthar40128@gmail.com>
2023-10-15 16:31:14 +0530

    Shree Balaji Recipes

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import { NextApiHandler } from 'next';
 import { AuthOptions } from "../../util";
+import { GET } from "../../recepi/route";
 const handler: NextApiHandler = NextAuth(AuthOptions);
-export default handler;
+export { handler as GET, handler as POST };
